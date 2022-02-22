f479cb7cf01ffa44042d05028dd77efcdf7f0356
천영환 <zing0982@naver.com>
2022-02-22 18:19:36 +0900

    useEffect_Practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import Button from "./Button"
 import styles from "./App.module.css"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((counter) => counter + 1)
+  const onChange = (event) => setKeyword(event.target.value)
+
+  console.log("run all time")
+
+  useEffect(() => {
+    console.log("call the api")
+  }, [])
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("search for", keyword)
+    }
+  }, [keyword])
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome</h1>
-      <Button text={"Continure"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Serach here..."
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   )
 }
