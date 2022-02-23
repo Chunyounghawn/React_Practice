6917058fe0b0438d74c742ed3a921aae786fc541
천영환 <zing0982@naver.com>
2022-02-23 23:14:29 +0900

    .

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div>
-      <h1>The Coins! {coins.length}</h1>
+      <h1>The Coins : {coins.length}</h1>
       <input
         onChange={onChange}
         value={myMoney}
