cf31f8cc63b7966b6bd0162257f96b196c3065d5
천영환 <zing0982@naver.com>
2022-02-23 22:14:51 +0900

    toDoList_review

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { useState, useEffect } from "react"
 
 function App() {
   const [toDo, setToDo] = useState("")
+  const [toDos, setTodos] = useState([])
   const onChange = (event) => setToDo(event.target.value)
   const onSubmit = (event) => {
     event.preventDefault()
     if (toDo === "") {
       return
     }
-    console.log(toDo)
     setToDo("")
+    setTodos((currentArray) => [toDo, ...currentArray])
   }
+  console.log(toDos)
   return (
     <div>
+      <h1>my todos {toDos.length}</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -20,8 +23,14 @@ function App() {
           type="text"
           placeholder="write plz"
         />
-        <button>Add To Do</button>
+        <button> Add to do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   )
 }
