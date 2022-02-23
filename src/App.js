e0c00d32f010f45391ea8a5243794ed04090e12e
천영환 <zing0982@naver.com>
2022-02-23 23:14:00 +0900

    upgrade_coin_viewer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 function App() {
   const [loading, setLoading] = useState(true)
   const [coins, setCoins] = useState([])
+  const [myMoney, setMyMoney] = useState([])
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -13,17 +14,35 @@ function App() {
       })
   }, [])
 
+  function onChange(event) {
+    setMyMoney(event.target.value)
+  }
+
   return (
     <div>
       <h1>The Coins! {coins.length}</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol} : ${coin.quotes.USD.price} USD)
-          </li>
-        ))}
-      </ul>
+      <input
+        onChange={onChange}
+        value={myMoney}
+        type="number"
+        placeholder="write your USD"
+      />
+
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li>
+              {coin.name} {coin.symbol} :{" "}
+              {Math.round((myMoney / coin.quotes.USD.price) * 100) / 100}
+              {"  개의 "}
+              {coin.symbol}
+              {" 구매가능!"}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
