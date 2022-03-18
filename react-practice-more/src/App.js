cba74ddde6066342580778e7421ed3f92a536e3a
Chunyounghawn <zing0982@naver.com>
2022-03-18 17:58:51 +0900

    how to use styled keyframes

--- react-practice-more/src/App.js
+++ react-practice-more/src/App.js
@@ -1,24 +1,54 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const rotationAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+
 `
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  position: relative;
+  top: 28px;
+  left: 120px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 30px;
+    &:hover {
+      //& = span
+      font-size: 80px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😊</span>
+      </Box>
+    </Wrapper>
   )
 }
 
