2da2476d10c1a83608fc7a342652c2b3fc806f6d
천영환 <zing0982@naver.com>
2022-03-19 22:20:28 +0900

    .

--- react-practice-more/src/App.tsx
+++ react-practice-more/src/App.tsx
@@ -1,7 +1,39 @@
-import Circle from "./Circle"
+import React, { useState } from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  background-
+`
+const H1 = styled.h1``
 
 function App() {
-  return <div></div>
+  const [value, setValue] = useState("")
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event
+    setValue(value)
+  }
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log("hello", value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
