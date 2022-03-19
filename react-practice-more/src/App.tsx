00958435f5d4e7a23ed01d7b490b4f7a0f0aeb4d
천영환 <zing0982@naver.com>
2022-03-19 20:49:25 +0900

    ts_state_practice

--- react-practice-more/src/App.tsx
+++ react-practice-more/src/App.tsx
@@ -1,27 +1,7 @@
-import styled, { keyframes } from "styled-components"
-
-const Title = styled.h1`
-  display: flex;
-  justify-content: 50px;
-  align-items: 50pxd;
-  color: ${(props) => props.theme.textColor};
-`
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`
+import Circle from "./Circle"
 
 function App() {
-  return (
-    <Wrapper>
-      <Title>HIHI</Title>
-    </Wrapper>
-  )
+  return <div></div>
 }
 
 export default App
--- react-practice-more/src/index.tsx
+++ react-practice-more/src/index.tsx
@@ -1,24 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { ThemeProvider } from "styled-components"
 
 import App from "./App"
 
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-}
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 )
