de8c40a94074ddfe7dedae79493a24111ea5bacf
천영환 <zing0982@naver.com>
2022-03-30 06:16:44 +0900

    during_add_redux

---
+++ react-practice-more/src/redux/actions/ThemeMode.ts
@@ -0,0 +1,14 @@
+export const DARK_THEME = 'DARK_THEME'
+export const LIGHT_THEME = 'LIGHT_THEME'
+
+export function DarkMode(){
+  return {
+    type:DarkMode
+  }
+}
+
+export function LigthMode(){
+  return {
+    type:LigthMode
+  }
+}
